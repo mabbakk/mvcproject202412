b80a84c584986333c50bab260eca343e8f84eea8
mabbakk <siyun145928@gmail.com>
2024-12-27 16:04:13 +0900

    Mybatis 기초 - PET Service 목록조회 구현

--- src/test/java/com/spring/mvcproject/database/mybatis/PetRepositoryTest.java
+++ src/test/java/com/spring/mvcproject/database/mybatis/PetRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,23 +18,41 @@ class PetRepositoryTest {
     @Test
     void saveTest() {
         Pet pet = Pet.builder()
-                .petName("초코초코")
-                .petAge(4)
+                .petName("꾸릉꾸릉")
+                .petAge(15)
                 .injection(true)
                 .build();
 
         petRepository.save(pet);
     }
 
-//    @Test
-//    void saveTest() {
-//        Pet pet = Pet.builder()
-//                .petName("둘기둘기")
-//                .patAge(1)
-//                .injection(false)
-//                .build();
-//
-//        petRepository.save(pet);
-//    }
+    @Test
+    void findIdTest() {
+        Pet foundPet = petRepository.findById(2L);
+
+        System.out.println("\n\nfoundPet = " + foundPet);
+    }
+
+    @Test
+    void findAllTest() {
+        List<Pet> petList = petRepository.findAll();
+
+        System.out.println("=========List of pet========");
+        petList.forEach(System.out::println);
+    }
+
+    @Test
+    void deleteByIdTest() {
+        petRepository.deleteById(2L);
+
+        Pet pet = petRepository.findById(2L);
+        System.out.println("pet = " + pet);
+    }
+
+    @Test
+    void countTest() {
+        int count = petRepository.petCount();
+        System.out.println("count = " + count);
+    }
 
 }
